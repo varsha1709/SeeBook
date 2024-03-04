67dad0db61e4e96c8fd2799aeff76e53f95d97f7
varsha1709 <91964033+varsha1709@users.noreply.github.com>
2024-03-05 01:02:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -311,6 +311,28 @@ def main():
         if st.button("Recommend"):
             recommended_books = recommend(user_input)
 
+    def recommend(user_input):
+    indices = np.where(pt.index == user_input)[0]
+    if len(indices) > 0:
+        index = indices[0]
+        similar_items = sorted(list(enumerate(similarity_score[index])), key=lambda x: x[1], reverse=True)[1:5]
+
+        data = []
+        for i in similar_items:
+            item = []
+            temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+
+            data.append(item)
+
+        # Display recommendations
+        for item in data:
+            st.write(item)
+    else:
+        st.write("Book not found. Please enter a valid book title.")
+
             if recommended_books:
                 st.subheader("Recommended Books")
                 for book in recommended_books:
